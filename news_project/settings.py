bf3ce4745df286d72bc43ea7881394e4cd940435
dyavathrocky <davathrak@gmail.com>
2020-06-07 21:46:31 +0530

    views by django...

--- news_project/settings.py
+++ news_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig', #new to add abstract user.
     'pages.apps.PagesConfig' , #to add pages app
+    'articles.apps.ArticlesConfig', # added articles app 
     'crispy_forms',  # added crispyforms to project
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -130,4 +132,12 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
